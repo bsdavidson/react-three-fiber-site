2d2c823a18a7d092fdbaf52aa0418b1958917ac0
Ryan King <ryan.king1809@gmail.com>
2019-03-21 14:08:27 +0100

    cleanup

--- doczrc.js
+++ doczrc.js
@@ -1,15 +1,5 @@
-import doczPluginNetlify from "docz-plugin-netlify";
-import { css } from 'docz-plugin-css'
-
 export default {
     src: './docs/',
     theme: 'theme/index',
     indexHtml: 'theme/index.html',
-    plugins: [
-        doczPluginNetlify(),
-        css({
-            preprocessor: 'sass',
-            cssmodules: true
-        })
-    ]
 }
